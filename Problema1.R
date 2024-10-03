6e251b266d0200e6d299538ef652166b1bb1f9bf
arnau.balust <arnau.balust@auleseebe.upc.edu>
2024-10-03 13:21:38 +0200

    qqqqq

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,17 @@
 #Codigo para problema 1
 
+#coeficient de determinacio  0.1923 el donen r^2
+r_sq <- 0.1923
+#coeficient de corrlacio 0.4385202 r
+sqrt(0.1923)
+
+#assets en lm 0.06576 es m
+m <- 0.06576
+#intercept en lm 11.79755 es b recta de regresio
+b <- 11.79755
+
+#prediccio sobre un punt x
+x <- 647.1
+y_pred <- m*x + b
+y_pred
+
--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,48 @@
 #Codigo para problema 2
+
+mis_dades <-iris
+mis_dades
+
+x <- mis_dades$Petal.Length
+x
+
+y <- mis_dades$Sepal.Length
+y
+
+plot(x,y)
+
+x_bar <- mean(x)
+x_bar
+
+y_bar <- mean(y)
+y_bar
+
+m <- sum((x-x_bar)*(y-y_bar))/sum((x-x_bar)^2)
+m
+
+b <- y_bar - m*x_bar
+b
+
+m*1.5 + b
+
+#prediccion sobre las observaciones
+y_pred <- m*x+b
+
+plot(x,y)
+lines(x,y_pred)
+
+#coeficiente determinacion
+#100% prediccion r^2 = 1
+#0% prediccion r^2 = 0
+r_sq <- sum((y_pred - y_bar)^2)/sum((y - y_bar)^2)
+r_sq
+
+
+###### usando las funciones de R
+#altgr 4 espacio ~
+mod <- lm(y~x)
+mod
+
+summary(mod)
+#coeficiente de correlacion
+cor.test(x,y)
